77298a36de6eae52d5580a57d8e532d465f18e40
Rojhon <rojhonbuenaventura5@gmail.com>
2026-07-17 19:09:33 +0800

    chore: adjust scraper workflow schedule

--- src/scraper.py
+++ src/scraper.py
@@ -2,7 +2,7 @@ def get_latest_release():
     return {
         "id": 1,
         "title": "Test Release",
-        "message": """🔥 New Episode!
+        "message": """🔥 This is Test for Automation New Episode!
 
 One Piece Episode 1171 is now available!
 
