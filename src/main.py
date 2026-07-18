8b82d94815100e255d704c870120f685c7aa04a8
Rojhon <rojhonbuenaventura5@gmail.com>
2026-07-18 09:24:16 +0800

    feat: improve manga recommendation post and update posting schedule

--- src/main.py
+++ src/main.py
@@ -4,30 +4,39 @@ from facebook import get_page, post_single_image_to_facebook_page
 
 
 page = get_page(FB_USER_TOKEN)
-random_manga = get_random_manga()
 
-title = (
-    random_manga["title"].get("english")
-    or random_manga["title"].get("romaji")
-)
+def random_manga():
+    random_manga = get_random_manga()
 
-message = f"""📚 Manga Recommendation of the Day!
+    title = (
+        random_manga["title"].get("english")
+        or random_manga["title"].get("romaji")
+    )
+
+    message = f"""📚 Manga Recommendation of the Day!
 
 🔥 {title}
 
-⭐ Score: {random_manga['averageScore']}/100
+Description: {random_manga['description']}
 
-Genres:
-{', '.join(random_manga['genres'])}
-
+Genres: {', '.join(random_manga['genres'])}
 
 """
 
-result = post_single_image_to_facebook_page(
-    page["page_id"],
-    page["page_token"],
-    message,
-    random_manga["coverImage"]["large"]
-)
+    result = post_single_image_to_facebook_page(
+        page["page_id"],
+        page["page_token"],
+        message,
+        random_manga["coverImage"]["large"]
+    )
 
-print(result)
+    # print(random_manga)
+
+def main():
+    random_manga()
+    # latest_anime_episode()
+    # character_spotlight()
+    # manga_news()
+
+if __name__ == "__main__":
+    main()
